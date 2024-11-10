94f75af041e91543e9ae48e141b9436e3d5d2b92
nithinanand04 <anand118@purdue.edu>
2024-11-10 00:45:57 -0500

    new db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,15 @@
 import { Sequelize } from "sequelize";
+import * as dotenv from 'dotenv';
 
-export const sequelize = new Sequelize( 'phase2_db', 'georgenolan', 'Maddie23!!', {
-  host: 'localhost',
+dotenv.config()
+const DB_NAME = process.env.DB_NAME ?? ''
+const DB_USER = process.env.DB_USER ?? ''
+const DB_PASS = process.env.DB_PASS ?? ''
+const DB_HOST = process.env.DB_HOST ?? ''
+
+
+export const sequelize = new Sequelize( DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
   dialect: 'postgres'
 });
 
