3106d50ac7ab6e769d43321e619e1bf8565c052c
nithinanand04 <anand118@purdue.edu>
2024-11-10 01:13:31 -0500

    sequelize

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -7,6 +7,10 @@ const DB_USER = process.env.DB_USER ?? ''
 const DB_PASS = process.env.DB_PASS ?? ''
 const DB_HOST = process.env.DB_HOST ?? ''
 
+console.log("DB_NAME:", DB_NAME);
+console.log("DB_USER:", DB_USER);
+console.log("DB_PASS:", DB_PASS ? "******" : "not set"); // Mask password for security
+console.log("DB_HOST:", DB_HOST);
 
 export const sequelize = new Sequelize( DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
